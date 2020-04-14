8b7c551b8bc22a54b292b9300df13939bbc12700
igormlg <igormlg@mail.ru>
2020-04-14 22:34:28 +0300

    Выгрузили проект сайта GMS

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("audio").on("play", function () {
+    $("audio").addClass("stoped").removeClass("playing");
+
+    $(this).removeClass("stoped").addClass("playing");
+    $(".stoped").each(function () {
+      $(this).trigger("pause");
+      $(this)[0].currentTime = 0;
+    });
+  });
+});
